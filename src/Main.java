60515c77622a688b468d18469c653fbf0ddb9cb2
Alfred Ortega <jusalfort07@byui.edu>
2021-01-30 23:23:01 +0800

    Response according to Step 07 of the assignment

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Maximiliano");
+
+        String response = "Hello Maxi, my name is Alfred ORtega";
+        System.out.println(response);
     }
 }
