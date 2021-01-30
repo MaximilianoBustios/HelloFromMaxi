75a01b80c6d636892fe231a649cd05d9e4a03c9e
camilacardus <camilacardus@byu.edu>
2021-01-30 12:19:58 -0300

    Added Camila's comment

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Maximiliano");
+        System.out.println("Hello, Maximiliano, my name is Camila");
     }
 }
